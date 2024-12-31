12a4f212f46f7b797acb52d9bfd5e64f3377f687
Francesco Culmone <fculmone5@gmail.com>
2024-12-31 01:04:53 +0000

    added a bit of padding to the bottom of the clan search as I didn't realize the clan search box at the bottom was a bit cut off

--- app/clan-search/page.tsx
+++ app/clan-search/page.tsx
@@ -5,7 +5,7 @@ import NestedPage from "./nested_page";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center px-24">
+    <main className="flex min-h-screen flex-col items-center px-24 pb-4">
       <Suspense>
         <NestedPage getData={getClanList} />
       </Suspense>
